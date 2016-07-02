4504c44ff1543fffd750f9381a9de40ed6e71cd2
Jim <james.schmitz@gmail.com>
2016-07-01 21:09:32 -0400

    change remote name

--- conf.py
+++ conf.py
@@ -401,7 +401,7 @@ DEPLOY_COMMANDS = {
 # GITHUB_DEPLOY_BRANCH = 'gh-pages'
 
 # The name of the remote where you wish to push to, using github_deploy.
-GITHUB_REMOTE_NAME = 'ssh-origin'
+GITHUB_REMOTE_NAME = 'origin'
 
 # Where the output site should be located
 # If you don't use an absolute path, it will be considered as relative
